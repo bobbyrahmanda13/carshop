a4ce31358de535b43cdfe3d8ca8e50c02cded2fb
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-01-28 18:10:45 +0700

    adding programatic: navigation and nuxtlink component

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const city = ref("");
+const cityError = ref(false);
+// const router = useRouter(); => menggunakan useRouter composable default vue 3
+
+const handleSearch = () => {
+  if (!city.value) {
+    return (cityError.value = true);
+  }
+  navigateTo(`/city/${city.value}/car`); // => routing bawaan nuxt 3
+  // router.push(`/city/${city.value}/car`); => menggunakan lanjutan dari useRouter
+};
+</script>
+
 <template>
   <!-- HOME SEARCH BAR -->
   <div
@@ -6,9 +20,13 @@
     <input
       type="text"
       class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+      :class="cityError ? 'border-red-500 border' : ''"
       placeholder="Search by city... "
+      v-model="city"
     />
-    <button class="bg-sky-500 px-10 text-white">Search</button>
+    <button class="bg-sky-500 px-10 text-white" @click="handleSearch">
+      Search
+    </button>
   </div>
   <!-- HOME SEARCH BAR -->
 </template>
