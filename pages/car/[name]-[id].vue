fbb40a9bed1b2f3261cc3b6167af227d0a602153
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-01-29 21:01:05 +0700

    add title to car detail

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const route = useRoute();
+useHead({
+  title: titleCaseWord(route.params.name as string),
+});
+function titleCaseWord(word: string) {
+  if (!word) return word;
+  return word[0].toUpperCase() + word.substring(1).toLowerCase();
+}
+</script>
 <template>
   <div>
     <NavBar />
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -2,17 +2,21 @@
 const route = useRoute();
 
 useHead({
-  title: `Car in ${titleCaseWord(route.params.city as string)}`,
+  title: `${
+    route.params.make ? titleCaseWord(route.params.make as string) : "Cars"
+  } in ${titleCaseWord(route.params.city as string)}`,
 });
-
+// function toTitleCase untuk membuat title huruf awalnya jadi huruf besar
 // function toTitleCase(str) {
 //   return str.replace(/\w\S*/g, function (txt: string) {
 //     return txt.charAt(0).toUpperCase() + txt.substr(1);
 //   });
 // }
+
+// function titleCaseWord untuk membuat title huruf awalnya jadi huruf besar
 function titleCaseWord(word: string) {
   if (!word) return word;
-  return word[0].toUpperCase() + word.substr(1).toLowerCase();
+  return word[0].toUpperCase() + word.substring(1).toLowerCase();
 }
 </script>
 <template>
