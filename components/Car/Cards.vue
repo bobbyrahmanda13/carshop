6eb9613da14537f3d8c2100762e6c3067a99658c
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-02-27 20:40:43 +0700

    forcing client side rendering using ClientOnly

--- components/Car/Cards.vue
+++ components/Car/Cards.vue
@@ -16,12 +16,14 @@ const handleFavorite = (id) => {
 </script>
 <template>
   <div class="w-full">
-    <CarCard
-      v-for="car in cars"
-      :key="car.id"
-      :car="car"
-      @favor="handleFavorite"
-      :favored="car.id in favorite"
-    />
+    <ClientOnly>
+      <CarCard
+        v-for="car in cars"
+        :key="car.id"
+        :car="car"
+        @favor="handleFavorite"
+        :favored="car.id in favorite"
+      />
+    </ClientOnly>
   </div>
 </template>
