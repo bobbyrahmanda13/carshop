a424848208c1817449f4e191477cf33ab416b982
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-02-27 20:43:50 +0700

    cara lain delete ClientOnly dan rename card to card.client.vue

--- components/Car/Cards.vue
+++ components/Car/Cards.vue
@@ -16,14 +16,12 @@ const handleFavorite = (id) => {
 </script>
 <template>
   <div class="w-full">
-    <ClientOnly>
-      <CarCard
-        v-for="car in cars"
-        :key="car.id"
-        :car="car"
-        @favor="handleFavorite"
-        :favored="car.id in favorite"
-      />
-    </ClientOnly>
+    <CarCard
+      v-for="car in cars"
+      :key="car.id"
+      :car="car"
+      @favor="handleFavorite"
+      :favored="car.id in favorite"
+    />
   </div>
 </template>
