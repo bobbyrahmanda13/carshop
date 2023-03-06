3196c2f6ce65d2e25c158b7fae293d486d59c271
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-03-06 21:16:24 +0700

    create view button link to message card

--- components/Car/ListingCard.vue
+++ components/Car/ListingCard.vue
@@ -15,7 +15,7 @@ const props = defineProps({
     <div class="p-3 flex">
       <NuxtLink
         class="text-blue-400 mr-5"
-        :to="`/profile/listing/view/${listing.id}`"
+        :to="`/profile/listings/view/${listing.id}`"
         >View</NuxtLink
       >
       <p class="text-red-400 cursor-pointer">Delete</p>
---
+++ components/Car/MessageCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="even:bg-gray-100 p-4">
+    <h1 class="font-bold text-2xl">Rahman Gabut</h1>
+    <p class="text-gray-700">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, ratione
+      quibusdam. Nulla culpa aperiam perspiciatis quo distinctio animi cum
+      cumque sunt aliquam doloremque.
+    </p>
+    <div class="flex items-center mt-4">
+      <p class="text-green-700 mr-10">rahmangabut@tutanota.com</p>
+      <p class="text-blue-500">34534-123412-231</p>
+    </div>
+  </div>
+</template>
