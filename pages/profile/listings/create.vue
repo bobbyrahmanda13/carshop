320f4ceede3ddbd379c30f248f1e9bb42aad2509
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-03-06 21:42:10 +0700

    create select options component

---
+++ pages/profile/listings/create.vue
@@ -0,0 +1,18 @@
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+
+const { makes } = useCars();
+</script>
+
+<template>
+  <div>
+    <div class="mt-24">
+      <h1 class="text-6xl">Create New Listing</h1>
+    </div>
+    <div class="shadow rounded p-4 mt-5 flex flex-wrap justify-between">
+      <CarAdSelect title="Make *" :options="makes" />
+    </div>
+  </div>
+</template>
