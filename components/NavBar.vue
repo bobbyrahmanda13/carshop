dc3601d6d1ee0559b5140a7d9f96e9f4953bd8c1
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-03-14 21:10:49 +0700

    Conditionally Rendering UI Based on Auth Status

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,18 @@
+<script setup>
+const user = useSupabaseUser();
+</script>
+
 <template>
   <!-- NAVBAR -->
   <header
     class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
   >
     <NuxtLink class="text-3xl font-mono" href="/">carshop</NuxtLink>
+    <div class="flex" v-if="user">
+      <NuxtLink to="profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
   <!-- NAVBAR -->
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -5,7 +5,7 @@ definePageMeta({
 
 const supabase = useSupabaseClient();
 
-const user = useSupabaseUser();
+// const user = useSupabaseUser();
 
 const login = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
@@ -19,7 +19,7 @@ const login = async () => {
 
 <template>
   <div class="mt-10">
-    {{ user }}
+    <!-- {{ user }} -->
     <h1 class="text-5xl font-bold mb-7">Log in</h1>
     <button @click="login" class="bg-red-400 p-3 rounded text-white font-bold">
       Login with Discord
